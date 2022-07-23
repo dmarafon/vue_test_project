0ecff913000d2bf356c7cf8ec4804c8a94011be2
Daniell Marafon <daniellmarafon@gmail.com>
2022-07-23 19:23:02 +0200

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["plugin:vue/essential", "airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+  },
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+# Vue Small Test Project
