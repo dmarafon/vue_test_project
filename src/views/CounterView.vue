352c14c9cef6acd65d22832f0f93c64f02286e95
Daniell Marafon <daniellmarafon@gmail.com>
2022-07-24 19:14:47 +0200

    Adding a simple button component with props for test

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
+import { watch } from "vue";
 import useCounterStore from "../stores/counterStore";
 
 const counterStore = useCounterStore();
+
+watch([counterStore], () => console.log("wowww"));
+
+const reset = () => {
+  counterStore.$reset();
+};
 </script>
 
 <template>
@@ -10,5 +17,6 @@ const counterStore = useCounterStore();
     <button @click="counterStore.incrementBy(1)">+1</button>
     <button @click="counterStore.incrementBy(2)">+2</button>
     <button @click="counterStore.incrementBy(3)">+3</button>
+    <button @click="reset">Reset</button>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
+import { watch } from "vue";
 import useCounterStore from "../stores/counterStore";
+import ButtonTest from "../components/ButtonTest.vue";
 
 const counterStore = useCounterStore();
+
+watch([counterStore.counter], () => console.log("wowww"));
 </script>
 
 <template>
   <div>
     <h1>Home View: {{ counterStore.counter }}</h1>
     <h2>Number of Changes {{ counterStore.numberOfChanges }}</h2>
+    <ButtonTest buttonName="test"></ButtonTest>
   </div>
 </template>
