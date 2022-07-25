bdeaa1e68becbde5a7aaf632f635d8a8ae4760b4
Daniell Marafon <daniellmarafon@gmail.com>
2022-07-25 03:34:44 +0200

    Adding further more packaged and configs

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -11,6 +11,16 @@ const reset = () => {
 };
 </script>
 
+<script lang="ts">
+export default {
+  data() {
+    return {
+      test: true,
+    };
+  },
+};
+</script>
+
 <template>
   <div>
     <h1>Counter View: {{ counterStore.counter }}</h1>
@@ -18,5 +28,12 @@ const reset = () => {
     <button @click="counterStore.incrementBy(2)">+2</button>
     <button @click="counterStore.incrementBy(3)">+3</button>
     <button @click="reset">Reset</button>
+    <button @click="test = !test">SHOW IMAGE</button>
+    <img
+      v-if="test"
+      :width="400"
+      src="https://firebasestorage.googleapis.com/v0/b/desterra-181ac.appspot.com/o/16549876665262.jpeg?alt=media&token=f93124ca-8568-436f-a7a5-b46b0d9eca5a"
+      alt="artwork from jesus perea"
+    />
   </div>
 </template>
